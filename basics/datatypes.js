47434f7020a5b2466e2c4941d87cd434ed2a3007
Chris <88323267+Chakshit01@users.noreply.github.com>
2024-02-14 12:36:03 +0000

    changes made

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -5,4 +5,11 @@ let isLogged = false;
 let ph = null;
 let address;
 
-console.log()
+
+console.log(typeof(address));// will display undefined 
+// undefined basicallly defines the uninitialized variable 
+console.log(typeof(ph))// will display object
+// whereas null depicts that the value is implicitly absent that no value is assigned to the variable
+
+// loosely equal : just checks the value not the datatype
+console.log(null == undefined);
---
+++ basics/typeConversions.js
@@ -0,0 +1,18 @@
+let name = "232";
+let str = "323acg";
+console.log(typeof(name));
+// datatype Conversions
+let Name = Number(name);
+let Str = Number(str); 
+// will convert both the datatypes of both
+console.log(typeof(Name));
+console.log(typeof(Str));
+
+console.log(Str);// will display value NaN which means not a number which itself is of Number datatype 
+
+console.log(Name);// will display the value as the string was easily convertable to the Number datatype
+
+
+let isApplicable = "";
+let isVariable = "not"
+let isApp = Boolean(isApplicable);
